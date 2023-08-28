299392914d67f148989efe069e98be26f656df7e
Pranali Patil <79775920+pranali18-Ai@users.noreply.github.com>
2023-08-28 11:49:37 +0530

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -3,14 +3,14 @@ export const themes = {
     title_color: "2f80ed",
     icon_color: "4c71f2",
     text_color: "434d58",
-    bg_color: "fffefe",
+    bg_color: "000000",
     border_color: "e4e2e2",
   },
   default_repocard: {
     title_color: "2f80ed",
     icon_color: "586069", // icon color is different
     text_color: "434d58",
-    bg_color: "fffefe",
+    bg_color: "000000",
   },
   transparent: {
     title_color: "006AFF",
